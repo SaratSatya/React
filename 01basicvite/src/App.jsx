de7176248f9d3e921dea3f1195eb1ccc35cfb352
Sarat Satya <surapaneneisaratsatya@gmail.com>
2025-10-16 16:41:15 +0530

    Basic projects

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <>
+    <a href="https://google.com" target="_blank">Click me to visit Google</a>
+    </>
+  )
+}
+
+export default App
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [counter,setCounter]=useState(15);
+  // let counter=15;
+  const addValue=()=>{
+    setCounter((prevCounter)=>prevCounter+1);
+    setCounter((prevCounter)=>prevCounter+1);
+  }
+  const removeValue=()=>{
+    setCounter((prevCounter)=>prevCounter-1);
+    setCounter((prevCounter)=>prevCounter-1);
+  }
+  return (
+    <>
+    <h1>React course with Counter {counter}</h1>
+    <h2>Counter value:{counter}</h2>
+    <button onClick={addValue}>Add value</button>
+    <button onClick={removeValue}> Remove value</button>
+    <p>Footer:{counter}</p>
+    </>
+  )
+}
+
+export default App
